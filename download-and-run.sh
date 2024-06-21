002ca5a67b54986e9daa45805bfe9eaacec769d5
Rafael Oliveira <rafaeloliveira@Mac-mini-de-Rafael.local>
2024-06-21 11:52:53 -0300

    Correção no Processo

--- download-and-run.sh
+++ download-and-run.sh
@@ -3,15 +3,22 @@
 # Defina a URL do script principal
 SCRIPT_URL="https://raw.githubusercontent.com/rafaeloliveiraz/WP-Plugin-Starter/main/create-plugin.sh"
 
-# Baixe o script principal para um diretório temporário
-TEMP_SCRIPT=$(mktemp)
+# Crie um diretório temporário para executar o script
+TEMP_DIR=$(mktemp -d)
+TEMP_SCRIPT="$TEMP_DIR/create-plugin.sh"
+
+# Baixe o script principal para o diretório temporário
 curl -s $SCRIPT_URL -o $TEMP_SCRIPT
 
 # Dê permissão de execução ao script baixado
 chmod +x $TEMP_SCRIPT
 
+# Navegue até o diretório temporário
+cd $TEMP_DIR
+
 # Execute o script principal
 $TEMP_SCRIPT
 
-# Remova o script temporário
-rm -f $TEMP_SCRIPT
+# Remova o diretório temporário após a execução
+cd -
+rm -rf $TEMP_DIR
